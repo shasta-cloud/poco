8d34d3a30317e15cda68ff0297018117c26427c0
Karthick Palanisamy <Karthick.palanisamy@truminds.com>
2024-09-06 06:57:03 +0000

    Debug print for IP filter

--- Net/src/SocketImpl.cpp
+++ Net/src/SocketImpl.cpp
@@ -375,11 +375,19 @@ int SocketImpl::receiveBytes(void* buffer, int length, int flags)
 	checkBrokenTimeout(SELECT_READ);
 
 	int rc;
+
+SocketAddress peerAddr = peerAddress();
+std::string addr = peerAddr.host().toString();
+
 	do
 	{
 		if (_sockfd == POCO_INVALID_SOCKET) throw InvalidSocketException();
 		rc = ::recv(_sockfd, reinterpret_cast<char*>(buffer), length, flags);
-        printf("%s %d past ::recv ret is %d errno is %d length %d \n", __func__, __LINE__, rc, lastError(), length);
+        if (addr == "122.176.39.60" || addr == "72.139.48.174")
+        {
+            printf("%s %d past ::recv ret is %d errno is %d length %d \n", __func__, __LINE__, rc, lastError(), length);
+        }
+
 	}
 	while (_blocking && rc < 0 && lastError() == POCO_EINTR);
 	if (rc < 0)
